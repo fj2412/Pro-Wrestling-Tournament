62768c78e41cc5dd9e5339965066159c81785ec7
fengj <fengjiang1009@gmail.com>
2023-12-08 23:40:57 -0500

    feat: implementation completed

--- wrestling-game.js
+++ wrestling-game.js
@@ -1,30 +1,101 @@
 const fs = require('fs');
+const { promisify } = require('util');
 
-function wrestlingGame() {
+async function wrestlingGame() {
     const filePath = 'data.json';
     let wrestlers = [];
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading the file:', err);
-            return;
-        }
-        try {
-            const jsonData = JSON.parse(data);
-            console.log('Data:', jsonData);
-            wrestlers = jsonData;
-            wrestlers.forEach(wrestler => {
-                console.log('Wrestler:', wrestler.name);
-                wrestler.moves.forEach(move => {
-                    console.log('Move:', move.name, '- Damage:', move.damage, '- Type:', move.type);
-                });
-                console.log(wrestler.moves[Math.floor(Math.random() * wrestler.moves.length)]);
-            });
-        } catch (err) {
-            console.error('Error during parsing JSON:', err);
-        }
-    });
+    const readFileAsync = promisify(fs.readFile);
+    try {
+        const data = await readFileAsync(filePath);
+        wrestlers = JSON.parse(data);
+    } catch (err) {
+        console.error('Error:', err);
+    }
 
+    let match = 1;
+    while (wrestlers.length !== 1) {
+        let wrestlerA = wrestlers.shift();
+        let wrestlerB = wrestlers.shift();
+        console.log("\nMatch " + match +":", wrestlerA.name, 'vs.', wrestlerB.name);
+        if (isEmpty(wrestlerA)) {
+            wrestlers.push(wrestlerB);
+            console.log(wrestlerB.name, "got a bye");
+        } else if (isEmpty(wrestlerB)) {
+            wrestlers.push(wrestlerA);
+            console.log(wrestlerA.name, "got a bye");
+        } else {
+            let freshA = JSON.parse(JSON.stringify(wrestlerA));
+            let freshB = JSON.parse(JSON.stringify(wrestlerB));
+            let round = 1;
+            while (wrestlerA.health > 0 && wrestlerB.health > 0) {
+                console.log("Round " + round + ":");
+                let finisherHitChance = Math.random();
+                if (round % 2 === 1) {
+                    // wrestler A turn
+                    let move = wrestlerA.moves[Math.floor(Math.random() * wrestlerA.moves.length)];
+                    if (move.type === 'finisher' && wrestlerB.health <= 45) {
+                        wrestlerB.health -= move.damage;
+                        console.log(wrestlerA.name, "performs", move.name, "on", wrestlerB.name);
+                        console.log(wrestlerB.name, "health:", wrestlerB.health);
+                        break;
+                    } else if (move.type === 'finisher' && finisherHitChance > 0.5) {
+                        wrestlerB.health -= move.damage;
+                        console.log(wrestlerA.name, "performs", move.name, "on", wrestlerB.name);
+                        console.log(wrestlerB.name, "health:", wrestlerB.health);
+                        break;
+                    } else if (move.type === 'finisher' && finisherHitChance <= 0.5) {
+                        console.log(wrestlerA.name, "fails to perform", move.name, "on", wrestlerB.name);
+                        console.log(wrestlerB.name, "health:", wrestlerB.health);
+                    } else {
+                        wrestlerB.health -= move.damage;
+                        console.log(wrestlerA.name, "performs", move.name, "on", wrestlerB.name);
+                        console.log(wrestlerB.name, "health:", wrestlerB.health);
+                    }
+                } else {
+                    // wrestler B turn
+                    let move = wrestlerB.moves[Math.floor(Math.random() * wrestlerB.moves.length)];
+                    if (move.type === 'finisher' && wrestlerA.health <= 45) {
+                        wrestlerA.health -= move.damage;
+                        console.log(wrestlerB.name, "performs", move.name, "on", wrestlerA.name);
+                        console.log(wrestlerA.name, "health:", wrestlerA.health);
+                        break;
+                    } else if (move.type === 'finisher' && finisherHitChance > 0.5) {
+                        wrestlerA.health -= move.damage;
+                        console.log(wrestlerB.name, "performs", move.name, "on", wrestlerA.name);
+                        console.log(wrestlerA.name, "health:", wrestlerA.health);
+                        break;
+                    } else if (move.type === 'finisher' && finisherHitChance <= 0.5) {
+                        console.log(wrestlerB.name, "fails to perform", move.name, "on", wrestlerA.name);
+                        console.log(wrestlerA.name, "health:", wrestlerA.health);
+                    } else {
+                        wrestlerA.health -= move.damage;
+                        console.log(wrestlerB.name, "performs", move.name, "on", wrestlerA.name);
+                        console.log(wrestlerA.name, "health:", wrestlerA.health);
+                    }
+                }
+                round++;
+            }
+            if (wrestlerA.health > wrestlerB.health) {
+                console.log(freshA.name, "wins!");
+                wrestlers.push(freshA);
+            } else {
+                console.log(freshB.name, "wins!");
+                wrestlers.push(freshB);
+            }
+        }
+        match++;
+    }
+    console.log("\n"+ wrestlers[0].name, "wins the tournament!");
 }
 
+function isEmpty(obj) {
+    for (const prop in obj) {
+      if (Object.hasOwn(obj, prop)) {
+        return false;
+      }
+    }
+  
+    return true;
+}
 
 wrestlingGame();
